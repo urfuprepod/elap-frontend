6592b6a1125a2409b52630b02005be9ac6939309
Ilya Papich <Mark.Zaharenko@ya.ru>
2025-06-15 22:24:57 +0500

    fix

--- src/shared/api/http-client/http-client.ts
+++ src/shared/api/http-client/http-client.ts
@@ -3,26 +3,27 @@ import axiosStatic, { AxiosInstance } from "axios";
 import { setupInterceptors } from "./http-client-interceptors";
 
 type HttpClient = {
-    axios: (baseURL?: string) => AxiosInstance;
+  axios: (baseURL?: string) => AxiosInstance;
 };
 
 const axiosInstance: AxiosInstance = axiosStatic.create();
 
 const axios = (baseURL?: string) => {
-    let instance: AxiosInstance = axiosInstance;
+  let instance: AxiosInstance = axiosInstance;
 
-    instance = axiosStatic.create({
-        withCredentials: true,
-    });
-    setupInterceptors(instance);
+  instance = axiosStatic.create({
+    // baseURL: "http://localhost:5500/api",
+    withCredentials: true
+  });
+  setupInterceptors(instance);
 
-    return instance;
+  return instance;
 };
 
 setupInterceptors(axiosInstance);
 
 export const httpClient: HttpClient = {
-    axios,
+  axios,
 };
 
 // export const BaseInstanse = axios.create({
